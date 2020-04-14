2870c582a826145c0b9a8e0499aa0cd4a6c7def3
labtec96 <cezaryhan1@gmail.com>
2020-04-14 21:48:36 +0200

    Added MapController and Entity Point

---
+++ src/main/java/com/example/controllers/MapController.java
@@ -0,0 +1,19 @@
+package com.example.controllers;
+
+import com.example.domain.Point;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by ch on 2020-04-14
+ */
+@Controller
+public class MapController {
+
+    @GetMapping("")
+    public String getMap(Model model){
+        model.addAttribute("point" , new Point(52.237049,21.017532,"hehe"));
+        return "map.html";
+    }
+}
---
+++ src/main/java/com/example/domain/Point.java
@@ -0,0 +1,21 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by ch on 2020-04-14
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+
+    private double lat;
+    private double lon;
+    private String text;
+
+}
